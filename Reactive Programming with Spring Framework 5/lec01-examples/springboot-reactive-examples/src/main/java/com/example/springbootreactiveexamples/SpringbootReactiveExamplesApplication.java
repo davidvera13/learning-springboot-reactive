29272dda5a320bb7205c26abb470f8d1c9d3d41e
davidvera13 <david.vera@9online.fr>
2023-03-12 20:43:30 +0100

    setup(reactive): setup example project

---
+++ Reactive Programming with Spring Framework 5/lec01-examples/springboot-reactive-examples/src/main/java/com/example/springbootreactiveexamples/SpringbootReactiveExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootreactiveexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactiveExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootReactiveExamplesApplication.class, args);
+    }
+
+}
---
+++ Reactive Programming with Spring Framework 5/lec01-examples/springboot-reactive-examples/src/test/java/com/example/springbootreactiveexamples/SpringbootReactiveExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootreactiveexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactiveExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
